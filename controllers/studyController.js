6be77eedf3928b3fbb55ce206c940e80d57cc961
hyemimi <hyemi7375@gmail.com>
2024-05-21 15:39:54 +0900

    add study

--- controllers/studyController.js
+++ controllers/studyController.js
@@ -1,18 +1,31 @@
+const { v4: uuidv4 } = require('uuid');
+var db = require('./../lib/db');
+
+
 const getUserStudies = async (req, res) => {
     // 특정 user의 스터디 리스트들을 불러옵니다
     try {
-
+        
     } catch (error) {
         res.send(error.message);
     }
 }
 
-const addStudies = async (req, res) => {
+const addStudies = async (request, response) => {
     // 새로운 스터디를 생성합니다
-    try {
+    var invite_code = uuidv4();
+    const {leader_id,description, title, banner,location,time} = request.body;
 
+    
+    try {
+     db.query('INSERT INTO study (invite_code,leader_id,description, title, banner,location,time) VALUES (?,?,?,?,?,?,?)', [invite_code,leader_id,description, title, banner,location,time], function(error, results, fields) {
+            if (error) throw error;
+            else {              
+              response.status(200).send(results.invite_code);
+            }           
+        });
     } catch (error) {
-        res.send(error.message);
+        response.send(error.message);
     }
 }
 
